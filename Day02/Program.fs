ac5a2e8f8ff59cdfd1304a34eecf52a31b589f17
Tom Rowsell <tom.rowsell@esendex.com>
2020-12-09 21:33:42 +0000

    Part One of day 2 done

--- Day02/Program.fs
+++ Day02/Program.fs
@@ -1,8 +1,8 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 open System
 
-
-type Policy = { BoundLetter : char ;   Occurance : int  }
+type Occurrence = { Lower:int; Upper:int }
+type Policy = { BoundLetter : char ;   Occurance : Occurrence  }
 type PolicyPwPair = { Policy : Policy ; Password : string }
 type StringSplit = { Chars : list<char> ; Index : int}
 
@@ -25,14 +25,58 @@ let splitString (s:string)(splitChar : char) =
             (String(List.toArray (List.rev result.Chars)))::(splitChars (List.skip result.Index a) splitChar)
     splitChars (List.ofArray (s.ToCharArray())) splitChar 
 
+let parsePasswordPolicy line = 
+    let parts = splitString line ' '
+    let parseRange = function
+    | hd :: _ ->
+        let parseRange = function
+        | lower::[upper]->
+            Some{Lower = lower; Upper = upper}
+   
+        | []|_ -> None
+        splitString hd '-'  |>  List.map(int) |> parseRange
+    | [] -> None
+    
+    let range = parseRange parts
+    {Password = List.last parts ; Policy = {BoundLetter = char (List.last(splitString (List.item 1 parts ) ':')) ; Occurance = range.Value } }  
 
 
+ 
+let rec recurseValidPasswords = function
+| hd::tl ->  
+    let rec passwordMeetsPolicy (policy:Policy) (index:int) = function
+    | hd::tl -> 
+        if List.isEmpty tl
+        then
+            let isValid index = (index>= policy.Occurance.Lower && index <= policy.Occurance.Upper)
 
+            if hd = policy.BoundLetter
+            then    
+                isValid (index + 1)
+            else
+                isValid index
+        else
+            if hd = policy.BoundLetter
+            then
+                
+                if (index + 1) > policy.Occurance.Upper
+                then
+                    false
+                else
+                    passwordMeetsPolicy policy (index + 1) tl
+            else
+                passwordMeetsPolicy policy (index) tl
+    |[] -> false
 
+    passwordMeetsPolicy hd.Policy 0 (Array.toList (hd.Password.ToCharArray())) :: recurseValidPasswords tl
+| [] -> List.empty
+
+    
 [<EntryPoint>]
 let main argv =
-    let passwords = System.IO.File.ReadAllLines("input.txt") |> List.ofArray
+    let passwords = System.IO.File.ReadAllLines("input.txt") |> List.ofArray 
 
-    let message = splitString "Hello.World.John.Jim" '.' 
-    message |> List.iter (fun s -> printfn "%s" s) 
+    let policies = passwords |> List.map parsePasswordPolicy 
+    let validCount =  List.where (id) (recurseValidPasswords policies)  
+    printfn "number of valid passwords is : %d" (List.length validCount)
     0 // return an integer exit code
