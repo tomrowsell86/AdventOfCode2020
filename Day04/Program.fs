0d5d35e95d1101666b0cc9f7a14d11a9a7343361
Tom Rowsell <tom.rowsell@esendex.com>
2020-12-14 23:08:15 +0000

    Day4 part one done

--- Day04/Program.fs
+++ Day04/Program.fs
@@ -11,6 +11,9 @@ let main argv =
   
     let passportList xs =  
         let rec getPassports xs = 
+
+            let getLineFields (s:String) = Array.toList (s.Split(' ', StringSplitOptions.RemoveEmptyEntries))
+            let getBlockFields fields = List.collect getLineFields fields
             [
                 let nextEmptyIndex =
                     try                  
@@ -20,21 +23,24 @@ let main argv =
                      
                 if nextEmptyIndex = -1
                 then
-                    yield xs
+                    yield getBlockFields xs
                 else
                     let splitList = List.splitAt nextEmptyIndex xs
                     
-                    yield fst splitList
+
+                    let fields = fst splitList |> getBlockFields 
+
+                    yield fields 
                     if not (List.isEmpty (snd splitList))
                     then
                         yield! getPassports  (List.skip 1 (snd splitList))
             ]
 
         getPassports xs
+    let validFilter fields = List.length fields = 8 || (not (List.exists (fun (s:String) -> s.StartsWith("cid")) fields) && List.length fields = 7)
+    let validPassports = passportList (Seq.toList (System.IO.File.ReadLines("input.txt"))) |> List.where validFilter
 
-    let passports = passportList (Seq.toList (System.IO.File.ReadLines("input.txt")))
-    
 
     let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    printfn "Valid passport count %d" (List.length validPassports)
     0 // return an integer exit code
