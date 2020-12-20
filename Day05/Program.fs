d8006f2b14530e564f430a59b79548ba8fdac3cb
Tom Rowsell <rowselltom@gmail.com>
2020-12-20 00:08:26 +0000

    Day5-part2 done

--- Day05/Program.fs
+++ Day05/Program.fs
@@ -15,13 +15,33 @@ let rec spaceDivide (lower:int) (upper:int) lowerChar = function
 let getRow (rowInstructions : list<char>) = spaceDivide 0 127 'F' rowInstructions
 let getAisle (ailseInstructions: list<char>) = spaceDivide 0 7 'L' ailseInstructions 
 
-let mapper (s : string)= 
+let mapper (s : string) : (int*int*int)= 
     let instructionTuple = Array.toList (s.ToCharArray()) |> List.splitAt 7  
-    getRow (fst instructionTuple) * 8 + getAisle (snd instructionTuple)
+    let getSeatNumbers (row,aisle) = (row, aisle, row * 8 + aisle )
+    let row = getRow (fst instructionTuple) 
+    let aisle = getAisle (snd instructionTuple)
+    (row, aisle, row * 8 + aisle)
     
 [<EntryPoint>]
 let main _ =
     let seatIds = List.map mapper boardingPasses 
-    List.iter (printfn "%d") seatIds
-    printfn "Highest SeatId is %d" (List.max seatIds)
-    0 // return an integer exit code
+               |> List.sortBy (fun (_,_,seatNo) -> seatNo) 
+    let (_,_,maxSeatNumber) = (List.maxBy (fun(_,_,seatNumber) -> seatNumber) seatIds)
+
+    let findGap (seatNumbers : list<int>) = 
+        let rec loop last = function 
+        | hd::tl -> if hd - last > 1
+                    then
+                        hd - 1
+                    else
+                        loop hd tl
+        | [] -> raise (Exception "No missing seats found")
+        loop (List.head seatNumbers) (List.skip 1 seatNumbers)
+
+    List.iter (fun (r,a,n) -> (printfn "%d %d %d") r a n) seatIds
+
+    let yourMissingSeat = List.map (fun (_,_,seatId) -> seatId) seatIds |>  findGap
+
+    printfn "Highest SeatId is %d" maxSeatNumber
+    printfn "Your missing seat is %d" yourMissingSeat
+    0 
