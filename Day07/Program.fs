1ce09271c5baad451d45ca5530a2a708ac247a42
Tom Rowsell <rowselltom@gmail.com>
2020-12-23 23:28:40 +0000

    Day07b latest

--- Day07/Program.fs
+++ Day07/Program.fs
@@ -5,9 +5,33 @@ open System
 // Define a function to construct a message to print
 let from whom =
     sprintf "from %s" whom
-
+type BagRule = {Container:string;Bags : list<string>}
 [<EntryPoint>]
 let main argv =
     let message = from "F#" // Call the function
+    let bagList = System.IO.File.ReadLines("input.txt") |> Seq.toList
+    let sanitiseBagString s = String.filter (fun c -> c <> '.') s
+    let parseRule (line:string) = 
+        let parts = line.Split("contains")
+        {
+            Container =  (parts |> Array.head); 
+            Bags = (parts |> Array.last |> fun s -> s.Split(',') |> Array.toList |> List.map sanitiseBagString) 
+         }
+    let rules = List.map parseRule bagList
+        
+    let rec loop bag containers = function
+    | hd::tl -> 
+        if  List.contains bag hd.Bags 
+        then
+            loop hd.Container (hd.Container::containers) rules
+        else
+            loop bag containers tl
+    | [] -> containers
+
+    let containers = loop "shiny gold bag" List.empty rules
+    printfn "Number of container bags is %d" (List.length containers) 
+
+
+
     printfn "Hello world %s" message
     0 // return an integer exit code
