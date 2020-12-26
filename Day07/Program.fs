3de0020c7ad7f68b00be6a7df323d658d420c94f
Tom Rowsell <rowselltom@gmail.com>
2020-12-26 23:29:22 +0000

    Day07 Part 1 done

--- Day07/Program.fs
+++ Day07/Program.fs
@@ -1,37 +1,30 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open BagRule
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
-type BagRule = {Container:string;Bags : list<string>}
 [<EntryPoint>]
 let main argv =
     let message = from "F#" // Call the function
     let bagList = System.IO.File.ReadLines("input.txt") |> Seq.toList
-    let sanitiseBagString s = String.filter (fun c -> c <> '.') s
-    let parseRule (line:string) = 
-        let parts = line.Split("contains")
-        {
-            Container =  (parts |> Array.head); 
-            Bags = (parts |> Array.last |> fun s -> s.Split(',') |> Array.toList |> List.map sanitiseBagString) 
-         }
+    
+    let filter = [0..9] |> List.map char |> List.append ['.']  
+    let sanitiseBagString s = String.filter (fun c -> not(List.contains c filter)) s |> (fun a -> a.Trim())
+
     let rules = List.map parseRule bagList
         
     let rec loop bag containers = function
     | hd::tl -> 
-        if  List.contains bag hd.Bags 
+        if  List.contains bag (List.map fst hd.Bags) 
         then
-            loop hd.Container (hd.Container::containers) rules
+            hd.Container::containers
+                @loop hd.Container List.empty rules
+                @loop bag containers tl
         else
             loop bag containers tl
     | [] -> containers
 
-    let containers = loop "shiny gold bag" List.empty rules
-    printfn "Number of container bags is %d" (List.length containers) 
+    let containers = loop "shiny gold" List.empty rules
+    printfn "Number of container bags is %d" (List.distinct containers |> List.length) 
 
-
-
-    printfn "Hello world %s" message
     0 // return an integer exit code
