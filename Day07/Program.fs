b774db3bfacce2f11c043385508349c12ade530c
Tom Rowsell <rowselltom@gmail.com>
2020-12-27 21:43:39 +0000

    All the bag fun in day07 part b

--- Day07/Program.fs
+++ Day07/Program.fs
@@ -1,11 +1,8 @@
-// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
-open System
 open BagRule
 
 [<EntryPoint>]
-let main argv =
-    let message = from "F#" // Call the function
+let main _ =
     let bagList = System.IO.File.ReadLines("input.txt") |> Seq.toList
     
     let filter = [0..9] |> List.map char |> List.append ['.']  
@@ -13,18 +10,34 @@ let main argv =
 
     let rules = List.map parseRule bagList
         
-    let rec loop bag containers = function
-    | hd::tl -> 
-        if  List.contains bag (List.map fst hd.Bags) 
-        then
-            hd.Container::containers
-                @loop hd.Container List.empty rules
-                @loop bag containers tl
-        else
-            loop bag containers tl
-    | [] -> containers
+    let getContainers bag rules =
+        let rec loop bag containers = function
+        | hd::tl -> 
+            if  List.contains bag (List.map fst hd.Bags) 
+            then
+                hd.Container::containers
+                    @loop hd.Container List.empty rules
+                    @loop bag containers tl
+            else
+                loop bag containers tl
+        | [] -> containers
+        loop bag List.empty rules
 
-    let containers = loop "shiny gold" List.empty rules
-    printfn "Number of container bags is %d" (List.distinct containers |> List.length) 
+    let getAllBagsFor container rules = 
+        let rec loop container bagTally = function
+        | hd::tl -> 
+            if hd.Container = container
+            then
+                let result = List.fold (fun a b ->  a + ((snd b) + (snd b) * (loop (fst b) List.empty  rules ))) 0  hd.Bags
+                printfn "Container : %s Result %d" container result
+                result
+            else
+                loop container bagTally tl   
+        | [] -> 0  
+        loop container List.empty rules
 
+    let containers = getContainers "shiny gold" rules
+    let numberOfBags = getAllBagsFor "shiny gold" rules
+    printfn "Part A : Number of container bags is %d" (List.distinct containers |> List.length) 
+    printfn "Part B : Number of container bags is %d" numberOfBags
     0 // return an integer exit code
