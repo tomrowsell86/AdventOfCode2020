0f4e0a7546c781fca1147a76f35b515eb078b809
Tom Rowsell <rowselltom@gmail.com>
2020-12-29 22:16:26 +0000

    Day 8 part 2 done

--- Day08/Program.fs
+++ Day08/Program.fs
@@ -2,12 +2,19 @@ open Shared.Regex
 open System
 
 
+let parseCommandLine line  =
+    match line with
+    | Regex "([a-z]{3})\s((\+|\-)\d+)" [command; arg; _] -> 
+        let operand = Int32.Parse(arg) 
+        Some(command,operand)
+    | _ -> None
+
 let initProcessing lines = 
     let rec loop behind ahead acc executedCommands = 
 
         let cmdId behindCount = 90000 - behindCount
 
-        let nextCommand acc offset (ahead : list<string>) (behind : list<string>) current= 
+        let nextCommand acc offset (ahead : list<string*int>) (behind : list<string*int>) current= 
             if offset < 0
             then
                 let next = (List.skip (List.length behind - (Math.Abs(offset))) behind)@[current]@ahead
@@ -22,26 +29,48 @@ let initProcessing lines =
         | hd::tl -> 
             if List.contains (cmdId(List.length behind)) executedCommands
             then
-                acc
+                (acc,false)
             else
-                match hd with
-                | Regex "([a-z]{3})\s((\+|\-)\d+)" [command; arg; _] -> 
-                    let operand = Int32.Parse(arg)
-                   
-
-                    match command with
-                    | "nop" -> nextCommand acc 1 tl behind  hd (cmdId (List.length behind)::executedCommands)
-                    | "acc" -> nextCommand (acc + operand) 1 tl behind hd (cmdId (List.length behind)::executedCommands) 
-                    | "jmp" -> nextCommand acc operand tl behind hd (cmdId (List.length behind)::executedCommands)
-                    | _ -> raise (exn "Unrecognised command")
-                | _ -> raise (exn "Input error")
-        | [] -> acc
+                let command = fst hd
+                let operand = snd hd
+                match command with
+                | "nop" -> nextCommand acc 1 tl behind  hd (cmdId (List.length behind)::executedCommands)
+                | "acc" -> nextCommand (acc + operand) 1 tl behind hd (cmdId (List.length behind)::executedCommands) 
+                | "jmp" -> nextCommand acc operand tl behind hd (cmdId (List.length behind)::executedCommands)
+                | _ -> raise (exn "Unrecognised command")
+        | [] -> (acc,true)
     loop List.empty lines  0 List.empty
 
 [<EntryPoint>]
 let main argv =
-    let lines = System.IO.File.ReadLines("input.txt") |> Seq.toList
-    let result = initProcessing lines
+    let parsedCommands = IO.File.ReadLines("input.txt") 
+                            |> Seq.map parseCommandLine
+                            |> Seq.where (fun c -> c <> None) 
+                            |> Seq.map (fun c -> c.Value) 
+                            |> Seq.toList
+    let (partA,_) = initProcessing parsedCommands
+    let partB commands =
+        let rec loop behind ahead = 
+            match ahead with
+            | hd::tl ->
+                let next = loop (behind@[hd])
+                let replaced = 
+                    match fst hd with 
+                    | "jmp" -> ("nop",snd hd)
+                    | "nop" when snd hd <> 0 ->  ("jmp", snd hd)
+                    | _ -> hd
+                if replaced <> hd
+                then
+                    let nextCmdCandidate = behind@[replaced]@(List.skip 1 ahead)
+                    let (acc, terminatedOnNoMoreInstructions) = initProcessing nextCmdCandidate
+                    if terminatedOnNoMoreInstructions
+                    then acc
+                    else next tl
+                else
+                    next tl
+            | [] -> raise (exn "Could not find instruction change to cause natural termination")
 
-    printfn "Part A result is : %d" result
+        loop List.empty commands    
+    printfn "Part A result is : %d" partA
+    printfn "Part B result is : %d" (partB parsedCommands)
     0 // return an integer exit code
